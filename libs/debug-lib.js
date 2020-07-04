49070dee8f435a9f62c757cb5ace6b87c75911ad
Jay <jvelayud@gmail.com>
2020-07-03 21:05:51 -0400

    Testing new debug lib

--- libs/debug-lib.js
+++ libs/debug-lib.js
@@ -1,16 +1,23 @@
-import AWS from "aws-sdk";
 import util from "util";
-
-// Log AWS SDK calls
-AWS.config.logger = { log: debug };
+import AWS from "aws-sdk";
 
 let logs;
+
+// Log AWS SDK calls
+AWS.config.logger = { log };
+
+export default function log() {
+  logs.push({
+    date: new Date(),
+    string: util.format.apply(null, arguments),
+  });
+}
 
 export function init(event, context) {
   logs = [];
 
   // Log API event
-  debug("API event", {
+  log("API event", {
     body: event.body,
     pathParameters: event.pathParameters,
     queryStringParameters: event.queryStringParameters,
@@ -20,11 +27,4 @@ export function init(event, context) {
 export function flush(e) {
   logs.forEach(({ date, string }) => console.debug(date, string));
   console.error(e);
-}
-
-export default function debug() {
-  logs.push({
-    date: new Date(),
-    string: util.format.apply(null, arguments),
-  });
 }
